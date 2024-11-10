eae9ed25876d25ce0871326a7f059dfab65207ca
Diogo Santos <up202108747@fe.up.pt>
2024-11-10 13:33:06 +0000

    switched template code for newer version

--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -4,18 +4,18 @@ import { MyFileReader } from './parser/MyFileReader.js';
 /**
  *  This class contains the contents of out application
  */
-class MyContents  {
+class MyContents {
 
     /**
        constructs the object
        @param {MyApp} app The application object
-    */ 
+    */
     constructor(app) {
         this.app = app
         this.axis = null
 
         this.reader = new MyFileReader(this.onSceneLoaded.bind(this));
-        this.reader.open("scenes/demo/demo.json");		
+        this.reader.open("scenes/demo/demo.json");
     }
 
     /**
@@ -31,63 +31,30 @@ class MyContents  {
     }
 
     /**
-     * Called when the scene xml file load is complete
-     * @param {MySceneData} data the entire scene data object
+     * Called when the scene JSON file load is completed
+     * @param {Object} data with the entire scene object
      */
     onSceneLoaded(data) {
-        console.info("scene data loaded " + data + ". visit MySceneData javascript class to check contents for each data item.")
+        console.info("YASF loaded.")
         this.onAfterSceneLoadedAndBeforeRender(data);
     }
 
-    output(obj, indent = 0) {
-        console.log("" + new Array(indent * 4).join(' ') + " - " + obj.type + " " + (obj.id !== undefined ? "'" + obj.id + "'" : ""))
-    }
-
-    onAfterSceneLoadedAndBeforeRender(data) {
-       
-        // refer to descriptors in class MySceneData.js
-        // to see the data structure for each item
-
-        this.output(data.options)
-        console.log("textures:")
-        for (var key in data.textures) {
-            let texture = data.textures[key]
-            this.output(texture, 1)
-        }
-
-        console.log("materials:")
-        for (var key in data.materials) {
-            let material = data.materials[key]
-            this.output(material, 1)
-        }
-
-        console.log("cameras:")
-        for (var key in data.cameras) {
-            let camera = data.cameras[key]
-            this.output(camera, 1)
-        }
-
-        console.log("nodes:")
-        for (var key in data.nodes) {
-            let node = data.nodes[key]
-            this.output(node, 1)
-            for (let i=0; i< node.children.length; i++) {
-                let child = node.children[i]
-                if (child.type === "primitive") {
-                    console.log("" + new Array(2 * 4).join(' ') + " - " + child.type + " with "  + child.representations.length + " " + child.subtype + " representation(s)")
-                    if (child.subtype === "nurbs") {
-                        console.log("" + new Array(3 * 4).join(' ') + " - " + child.representations[0].controlpoints.length + " control points")
-                    }
-                }
-                else {
-                    this.output(child, 2)
-                }
+    printYASF(data, indent = '') {
+        for (let key in data) {
+            if (typeof data[key] === 'object' && data[key] !== null) {
+                console.log(`${indent}${key}:`);
+                this.printYASF(data[key], indent + '\t');
+            } else {
+                console.log(`${indent}${key}: ${data[key]}`);
             }
         }
     }
 
+    onAfterSceneLoadedAndBeforeRender(data) {
+        this.printYASF(data)
+    }
+
     update() {
-        
     }
 }
 
